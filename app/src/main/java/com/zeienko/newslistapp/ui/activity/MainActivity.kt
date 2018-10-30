7966a5d774628acda26439ff9ad1443412af84a8
Vitaly Zeenko <vitaly.zeenko@chisw.com>
2018-10-30 14:41:32 +0200

    Create layouts for news list and for items in the list.

--- app/src/main/java/com/zeienko/newslistapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/zeienko/newslistapp/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.zeienko.newslistapp
+package com.zeienko.newslistapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zeienko.newslistapp.R
 
 class MainActivity : AppCompatActivity() {
 
